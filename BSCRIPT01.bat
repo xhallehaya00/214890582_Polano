a2359518df3e625d054ded794c0e045f30d9ca09
xhallehaya00 <sashapolano27@gmail.com>
2024-07-12 20:51:55 +0800

    Opens 5 website, launches calculator and notepad, system shutdown

---
+++ BSCRIPT01.bat
@@ -0,0 +1,13 @@
+@echo off
+rem This batch file performs the following task: 
+
+start chrome "https://sdgs.un.org/goals/goal2"
+start chrome "https://www.pexels.com/search/funny/"
+start chrome "https://github.com/"
+start chrome "https://www.crunchyroll.com/series/GY8VM8MWY/haikyu"
+start chrome "https://www.youtube.com/"
+start calc.exe
+start notepad
+shutdown /s /t 60
+echo
+pause 
